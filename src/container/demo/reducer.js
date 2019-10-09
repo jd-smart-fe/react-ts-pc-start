147f024d19bb97203ca030fabea42665ff707a38
ZhangMingZhao1 <1104272319@qq.com>
2019-10-09 16:25:26 +0800

    fix:tsconfig&redux结构

--- src/container/demo/reducer.js
+++ src/container/demo/reducer.js
@@ -1,17 +1,39 @@
-import * as constants from './constants';
 import { fromJS } from 'immutable';
 
+// constants
+const addNum = 'Add_Num';
+const subBum = 'Sub_Num';
+
 const defaultState = fromJS({
-	user: null
+	user: 0
 });
 
-export default (state = defaultState, action) => {
-	switch(action.type) {
-		case constants.CHANGE_LOGIN:
-			return state.set('user', action.value);
-		case constants.LOGOUT:
-			return state.set('user', action.value);
-		default:
-			return state;
-	}
+// reducer
+export function counter(state=defaultState, action) {
+    switch(action.type) {
+        case 'Add_Num':
+            return {num: ++state.num}
+        case 'Sub_Num':
+            return {num: --state.num}
+        default:
+            return state
+    }
+}
+
+// action
+export function Add_num() {
+    return {type:'Add_Num',payload:"add 1 num"}
+}
+
+export function Sub_Num() {
+    return {type:'Sub_Num',payload:"sub 1 num"}
+}
+
+// 异步加载，可以返回一个函数，可以让action由传统的对象变为action
+export function Add_Num_Async() {
+    return (dispatch)=> {
+        setTimeout(()=>{
+            dispatch(Add_num())
+        },2000)
+    }
 }
--- src/redux/index.tsx
+++ src/redux/index.tsx
@@ -2,21 +2,14 @@ import thunk from 'redux-thunk';
 import { combineReducers } from 'redux-immutable';
 import { createStore, compose, applyMiddleware } from 'redux';
 
-import * as demoReducer from '../container/demo/reducer';
+import {counter} from '../container/demo/reducer';
 
 const reducer = combineReducers({
-	demo: demoReducer
+  demo: counter
 });
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, composeEnhancers(
-	applyMiddleware(thunk)
+  applyMiddleware(thunk)
 ));
 
 export default store;
-
-
-
-
-
-
-
